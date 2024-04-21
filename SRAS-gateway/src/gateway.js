67cca9f232973303e92be0f049eb22385c38e466
Mina Magdy <minamagdy651@gmail.com>
2024-04-21 12:21:10 +0200

    test endpoints

--- SRAS-gateway/src/gateway.js
+++ SRAS-gateway/src/gateway.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cors = require("cors");
 
 const videoRoutes = require("./routes/videos");
+const testRoutes = require("./routes/testRoutes");
 
 const envFilePath = path.resolve(__dirname, "../../.env");
 dotenv.config({ path: envFilePath });
@@ -12,6 +13,8 @@ const app = express();
 app.use(cors());
 
 app.use("/video", videoRoutes);
+app.use("/test", testRoutes);
+
 const port = process.env.GATEWAY_PORT;
 
 app.listen(port, () => {
