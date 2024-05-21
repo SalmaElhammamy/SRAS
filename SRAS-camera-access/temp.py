959f687a8c11cf49f943e303938dfa913be058df
Mina Magdy <minamagdy651@gmail.com>
2024-05-22 00:33:28 +0300

    WIP

--- SRAS-camera-access/temp.py
+++ SRAS-camera-access/temp.py
@@ -1,37 +1,14 @@
-import os
-from flask import Flask, Response, jsonify, url_for
 import cv2
 
-app = Flask(__name__, static_folder='Images')
+video_path = 'DemoVideos/1.mp4'
 
+cap = cv2.VideoCapture(video_path)
 
-@app.route('/preview')
-def preview():
-    try:
-
-        image_lists = []
-        os.chdir('SRAS-camera-access')
-
-        for i in range(1, 4):
-            image_path = os.path.join('Images', 'test.png')
-
-            if os.path.exists(image_path):
-                image = cv2.imread(image_path)
-                height, width, _ = image.shape
-
-                image_url = url_for('static', filename='test.png')
-
-                image_lists.append({
-                    "image_url": image_url,
-                    "height": height,
-                    "width": width
-                })
-            else:
-                continue
-        return jsonify(image_lists)
-    except Exception as e:
-        return "An error occurred"
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+while True:
+    ret, frame = cap.read()
+    if ret == True:
+        cv2.imshow('Frame', frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    else:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
