9cc1720861f155876c35866945bf4cb580eadb98
Mina Magdy <minamagdy651@gmail.com>
2024-06-23 23:46:09 +0300

    Modified: file names

--- SRAS-live-feed/consumer.py
+++ SRAS-live-feed/consumer.py
@@ -3,7 +3,7 @@ import numpy as np
 import sys
 from kombu import Connection, Exchange, Queue
 from kombu.mixins import ConsumerMixin
-import multiprocessing
+import threading
 from flask import Flask, Response, jsonify, url_for
 import uuid
 import os
@@ -13,6 +13,19 @@ import torch
 from ultralytics import YOLO
 import supervision as sv
 
+from utils.general import find_in_list, load_zones_config
+from utils.timers import FPSBasedTimer
+
+import requests
+import json
+
+from dotenv import load_dotenv
+env_path = '../.env'
+load_dotenv(env_path)
+
+URL = os.getenv('URL')
+DB_SERVER_PORT = os.getenv('DB_SERVER_PORT')
+
 app = Flask(__name__, static_folder='Images')
 
 
@@ -21,17 +34,40 @@ class WorkerType(Enum):
     WITH_INFERENCE = 2
 
 
+tracker = sv.ByteTrack(minimum_matching_threshold=0.9)
+
+zone_configuration_path = './config.json'
+
+
+COLORS = sv.ColorPalette.from_hex(["#E6194B", "#3CB44B", "#FFE119", "#3C76D1"])
+COLOR_ANNOTATOR = sv.ColorAnnotator(color=COLORS)
+LABEL_ANNOTATOR = sv.LabelAnnotator(
+    color=COLORS, text_color=sv.Color.from_hex("#000000")
+)
+
+
+def get_coordinates(driverId):
+    try:
+        response = requests.get(
+            f'{URL}:{DB_SERVER_PORT}/camera/coordinates/{driverId}')
+        response = response.json()
+
+        nested_list = json.loads(response['coordinates'])
+        array_list = [np.array(sublist) for sublist in nested_list]
+        return array_list
+    except Exception as e:
+        print(e)
+        return None
+
+
 class Config:
     broker_url = 'amqp://guest:guest@localhost:5672//'
     camera_drivers = ['0', '1']
     routes = {driver: f"{uuid.uuid4()}" for driver in camera_drivers}
-    frames = multiprocessing.Manager().dict(
-        {driver: None for driver in camera_drivers})
-    inference_frames = multiprocessing.Manager().dict(
-        {driver: None for driver in camera_drivers})
-
-
-bounding_box_annotator = sv.BoundingBoxAnnotator()
+    frames = {driver: None for driver in camera_drivers}
+    inference_frames = {driver: None for driver in camera_drivers}
+    models = {driver: None for driver in camera_drivers}
+    polygons = {driver: get_coordinates(driver) for driver in camera_drivers}
 
 
 class Worker(ConsumerMixin):
@@ -62,25 +98,75 @@ class WorkerWithInference(Worker):
     def __init__(self, connection, queue, driver):
         super().__init__(connection, queue, driver)
         self.model = YOLO('yolov8n.pt')
+
         if torch.cuda.is_available():
             print(f"Model for {driver} is using GPU")
             self.model.to('cuda')
         else:
             print(f"Model for {driver} is using CPU")
 
+        self.get_coordinates()
+
+    def get_coordinates(self):
+        self.polygons = Config.polygons[self.driver]
+
+        self.zones = [
+            sv.PolygonZone(
+                polygon=polygon,
+                triggering_anchors=(sv.Position.CENTER,),
+            )
+            for polygon in self.polygons
+        ]
+        self.timers = [FPSBasedTimer(15) for _ in self.zones]
+
     def on_message(self, body, message):
         size = sys.getsizeof(body) - 33
         np_array = np.frombuffer(body, dtype=np.uint8)
         np_array = np_array.reshape((size, 1))
-        image = cv2.imdecode(np_array, 1)
+        frame = cv2.imdecode(np_array, 1)
 
-        result = self.model(
-            source=image,
-            verbose=False
+        results = self.model.predict(
+            source=frame,
+            verbose=False,
+            classes=[0],
+            conf=0.7,
+            iou=0.7
         )[0]
 
-        detections = sv.Detections.from_ultralytics(result)
-        annotated_frame = bounding_box_annotator.annotate(image, detections)
+        detections = sv.Detections.from_ultralytics(results)
+        detections = detections[find_in_list(detections.class_id, [0])]
+        detections = tracker.update_with_detections(detections)
+
+        annotated_frame = frame.copy()
+        for idx, zone in enumerate(self.zones):
+            annotated_frame = sv.draw_polygon(
+                scene=annotated_frame, polygon=zone.polygon, color=COLORS.by_idx(
+                    idx)
+            )
+            try:
+                detections_in_zone = detections[zone.trigger(detections)]
+            except:
+                continue
+
+            time_in_zone = self.timers[idx].tick(detections_in_zone)
+            custom_color_lookup = np.full(
+                detections_in_zone.class_id.shape, idx)
+
+            annotated_frame = COLOR_ANNOTATOR.annotate(
+                scene=annotated_frame,
+                detections=detections_in_zone,
+                custom_color_lookup=custom_color_lookup,
+            )
+            labels = [
+                f"#{tracker_id} {int(time // 60):02d}:{int(time % 60):02d}"
+                for tracker_id, time in zip(detections_in_zone.tracker_id, time_in_zone)
+            ]
+            annotated_frame = LABEL_ANNOTATOR.annotate(
+                scene=annotated_frame,
+                detections=detections_in_zone,
+                labels=labels,
+                custom_color_lookup=custom_color_lookup,
+            )
 
         _, jpeg_frame = cv2.imencode('.jpg', annotated_frame)
 
@@ -89,30 +175,32 @@ class WorkerWithInference(Worker):
         message.ack()
 
 
-class ConsumerProcess(multiprocessing.Process):
+class Consumer:
     def __init__(self, driver, worker_type=WorkerType.DEFAULT):
-        super().__init__()
         self.driver = driver
         self.worker_type = worker_type
         self.exchange = Exchange(driver + '-exchange', type='direct')
         self.queue = Queue(driver + '-queue', self.exchange,
                            routing_key="video")
 
-    def run(self):
+    def consume(self):
         with Connection(Config.broker_url) as connection:
             if self.worker_type == WorkerType.DEFAULT:
-                worker = Worker(connection, self.queue, self.driver)
+                self.worker = Worker(connection, self.queue, self.driver)
             else:
-                worker = WorkerWithInference(
+                self.worker = WorkerWithInference(
                     connection, self.queue, self.driver)
-            worker.run()
+            self.worker.run()
+
+    def update_coordinates(self):
+        self.worker.get_coordinates()
 
 
 @app.route('/preview')
 def preview():
     try:
         image_lists = []
-        os.chdir('SRAS-camera-access')
+        os.chdir('SRAS-live-feed')
 
         for camera_driver in Config.camera_drivers:
             file_name = f'preview-{camera_driver}.jpg'
@@ -136,6 +224,7 @@ def preview():
     except Exception as e:
         print(e)
         return "An error occurred"
+
     finally:
         os.chdir('..')
 
@@ -159,6 +248,15 @@ def video_feed_inference(driver_uuid):
     return Response(generate_frames(driver, worker_type=WorkerType.WITH_INFERENCE), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+@app.route('/update-coordinates')
+def update_coordinates():
+    Config.polygons = {driver: get_coordinates(
+        driver) for driver in Config.camera_drivers}
+    for inference_consumer in inference_consumers:
+        inference_consumer.update_coordinates()
+    return "Coordinates updated"
+
+
 def generate_frames(driver, worker_type=WorkerType.DEFAULT):
     while True:
         if worker_type == WorkerType.DEFAULT:
@@ -170,21 +268,16 @@ def generate_frames(driver, worker_type=WorkerType.DEFAULT):
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
+consumers = [Consumer(driver) for driver in Config.camera_drivers]
+inference_consumers = [Consumer(driver, WorkerType.WITH_INFERENCE)
+                       for driver in Config.camera_drivers]
+
 if __name__ == '__main__':
-    consumers = [ConsumerProcess(driver) for driver in Config.camera_drivers]
-    inference_consumers = [ConsumerProcess(driver, WorkerType.WITH_INFERENCE)
-                           for driver in Config.camera_drivers]
 
     for consumer in consumers:
-        consumer.start()
+        threading.Thread(target=consumer.consume).start()
 
     for inference_consumer in inference_consumers:
-        inference_consumer.start()
+        threading.Thread(target=inference_consumer.consume).start()
 
     app.run(host='0.0.0.0', debug=False)
-
-    for consumer in consumers:
-        consumer.join()
-
-    for inference_consumer in inference_consumers:
-        inference_consumer.join()
